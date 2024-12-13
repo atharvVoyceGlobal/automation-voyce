e0c6841b3b7a7f0041eb976a86b5a0a2f18de765
NikitaBars <146870036+NikitaBars@users.noreply.github.com>
2024-12-13 13:34:17 -0500

    Update Transaction_page_Admin.py

--- Admin_pages/Transaction_page_Admin.py
+++ Admin_pages/Transaction_page_Admin.py
@@ -2411,41 +2411,40 @@ class Transaction_page_A(Graphs, EV):
             print(f"Error performing arrow down and enter: {e}")
 
     def check_gmail(self, original_tab):
-        try:
-            self.driver.execute_script("window.open('');")
-            time.sleep(2)
+        self.driver.execute_script("window.open('');")
+        time.sleep(2)
 
-            # Переключение на новую вкладку
-            new_tab_index = self.driver.window_handles[-1]
-            self.driver.switch_to.window(new_tab_index)
+        # Переключение на новую вкладку
+        new_tab_index = self.driver.window_handles[-1]
+        self.driver.switch_to.window(new_tab_index)
 
-            # Переход на Gmail
-            self.driver.get(self.url1)
-            time.sleep(10)
+        # Переход на Gmail
+        self.driver.get(self.url1)
+        time.sleep(10)
 
-            # Проверка наличия поля для ввода логина
-            self.screenshot()
-            self.input_login(self.my_accaunt)
-            self.press_return_key()
-            time.sleep(3)
-            self.input_password(self.my_password)
-            time.sleep(3)
-            self.press_return_key()
-            self.screenshot()
+        # Проверка наличия поля для ввода логина
+        self.screenshot()
+        self.input_login(self.my_accaunt)
+        self.press_return_key()
+        time.sleep(3)
+        self.input_password(self.my_password)
+        time.sleep(3)
+        self.press_return_key()
+        self.screenshot()
 
-            time.sleep(20)
-            self.press_return_key()
-            time.sleep(5)
-            self.screenshot()
-            # self.click_element_center()
-            time.sleep(5)
-            self.click_db_in_mail()
-            time.sleep(1)
-            self.click_more_and_delete()
-            self.driver.close()
-            time.sleep(5)
-            self.driver.switch_to.window(original_tab)
-            time.sleep(10)
+        time.sleep(20)
+        self.press_return_key()
+        time.sleep(5)
+        self.screenshot()
+        # self.click_element_center()
+        time.sleep(5)
+        self.click_db_in_mail()
+        time.sleep(1)
+        self.click_more_and_delete()
+        self.driver.close()
+        time.sleep(5)
+        self.driver.switch_to.window(original_tab)
+        time.sleep(10)
 
 
     def compare_data_for_periods(self):
