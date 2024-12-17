57e0a36bc65287b0d9874a011ff163ba4174ae8b
NikitaBars <146870036+NikitaBars@users.noreply.github.com>
2024-12-17 17:20:09 -0500

    Update Transaction_page_Admin.py

--- Admin_pages/Transaction_page_Admin.py
+++ Admin_pages/Transaction_page_Admin.py
@@ -43,7 +43,6 @@ from customer_pages.Graph_c import Graphs
 from ev import EV
 
 
-
 def is_equivalent_service_minutes(db_value, web_value):
     if (db_value in [None, '-', 0] and web_value in ['', '-', '0']) or str(db_value) == web_value:
         return True
