b4eee81869f59c74765540e3910607d03dc9a7ba
NikitaBars <146870036+NikitaBars@users.noreply.github.com>
2025-02-27 15:49:19 -0500

    Update env.py

--- env.py
+++ env.py
@@ -1,17 +1,13 @@
 import os
 
 class EV:
-    # URLs
-    AGENT_URL = 'https://martti-agent.qa.cloudbreak.us/signIn'
-    CUSTOMER_URL = 'https://cloudbreak-customer-ui.qa.cloudbreak.us/'
-
-    AGENT_LOGIN = "nikita.barshchuk+qaterp3@equitihealth.com"
-    AGENT_PASSWORD = "Gomynkyl165432_"
-
-    AGENT_ALT_LOGIN = "nikita.barshchuk+qaterp4@equitihealth.com"
-    AGENT_ALT_PASSWORD = "Gomynkyl165432_"
-
-    OPERATOR_LOGIN = "nikita.barshchuk+qaoper@equitihealth.com"
-    OPERATOR_PASSWORD = "Gomynkyl165432_"
-
-    CUSTOMER_LOGIN = "8944PA00006"
+    
+    AGENT_URL = os.getenv('AGENT_URL')
+    CUSTOMER_URL = os.getenv('CUSTOMER_URL')
+    AGENT_LOGIN = os.getenv('AGENT_LOGIN')
+    AGENT_PASSWORD = os.getenv('AGENT_PASSWORD')
+    AGENT_ALT_LOGIN  = os.getenv('AGENT_ALT_LOGIN')
+    OPERATOR_LOGIN = os.getenv('OPERATOR_LOGIN')
+    AGENT_ALT_PASSWORD = os.getenv('AGENT_ALT_PASSWORD')
+    OPERATOR_PASSWORD = os.getenv('OPERATOR_PASSWORD')
+    CUSTOMER_LOGIN = os.getenv('CUSTOMER_LOGIN')
