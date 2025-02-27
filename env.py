4eb4f3af2963b1107cb8303ff3726fb5010f7cb2
NikitaBars <146870036+NikitaBars@users.noreply.github.com>
2025-02-27 15:40:06 -0500

    Create env.py

---
+++ env.py
@@ -0,0 +1,16 @@
+import os
+
+class EV:
+
+    AGENT_URL = 'https://martti-agent.qa.cloudbreak.us/signIn'
+    CUSTOMER_URL = 'https://cloudbreak-customer-ui.qa.cloudbreak.us/'
+    AGENT_LOGIN = "nikita.barshchuk+qaterp3@equitihealth.com"
+    AGENT_PASSWORD = "Gomynkyl165432_"
+
+    AGENT_ALT_LOGIN = "nikita.barshchuk+qaterp4@equitihealth.com"
+    AGENT_ALT_PASSWORD = "Gomynkyl165432_"
+
+    OPERATOR_LOGIN = "nikita.barshchuk+qaoper@equitihealth.com"
+    OPERATOR_PASSWORD = "Gomynkyl165432_"
+
+    CUSTOMER_LOGIN = "8944PA00006"
